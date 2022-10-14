3d8220aca37d37c1f1f5185b79a463c274cd505d
Krogmoe <krogmoe@gmail.com>
2022-10-14 12:02:09 -0600

    Moving files.

--- template/main.tf
+++ template/main.tf
@@ -4,7 +4,7 @@
 # *****************************************************************************
 
 terraform {
-  required_version = "1.2.5"
+  required_version = "1.3.2"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
--- template/rbac.tf
+++ template/rbac.tf
@@ -2,11 +2,11 @@
 #**************************************
 # Subscription Level RBAC Roles
 #**************************************
-resource "azurerm_role_assignment" "spn_ado" {
-  scope                = data.azurerm_subscription.current.id
-  role_definition_name = "Owner"
-  principal_id         = data.azuread_service_principal.spn_ado.id
-}
+# resource "azurerm_role_assignment" "spn_ado" {
+#   scope                = data.azurerm_subscription.current.id
+#   role_definition_name = "Owner"
+#   principal_id         = data.azuread_service_principal.spn_ado.id
+# }
 
 #**************************************
 # Resource Group Level RBAC Roles
