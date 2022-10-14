f993862b60b367d4f378036c063843fc55327490
Krogmoe <krogmoe@gmail.com>
2022-10-14 11:02:59 -0600

    New files.

---
+++ rbac.tf
@@ -0,0 +1,24 @@
+
+#**************************************
+# Subscription Level RBAC Roles
+#**************************************
+resource "azurerm_role_assignment" "spn_ado" {
+  scope                = data.azurerm_subscription.current.id
+  role_definition_name = "Owner"
+  principal_id         = data.azuread_service_principal.spn_ado.id
+}
+
+#**************************************
+# Resource Group Level RBAC Roles
+#**************************************
+
+
+#**************************************
+# Storage Account Level RBAC Roles
+#**************************************
+
+
+#**************************************
+# Key Vault RBAC Roles
+#**************************************
+
